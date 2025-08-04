100e5542d71f3044df16f90a466d0301e4e5acb6
PY-SENSEI <gauravjena48@gmail.com>
2025-08-04 17:32:38 +0530

    some changes

--- src/routes/transactionsRoute.js
+++ src/routes/transactionsRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { sql } from "../config/db.js";
-import {  createTransaction, deleteTranscation, getSummaryByUserId, getTransactionsByUserId } from "../controllers/transactionsController.js";
+import {  createTransaction, deleteTransaction, getSummaryByUserId, getTransactionsByUserId } from "../controllers/transactionsController.js";
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ router.get("/:userId", getTransactionsByUserId);
 
 router.post("/", createTransaction);
 
-router.delete("/:id", deleteTranscation);
+router.delete("/:id", deleteTransaction);
 
 router.get("/summary/:userId", getSummaryByUserId);
 
